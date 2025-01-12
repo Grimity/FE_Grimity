8208371e426a344cf8536b837b5df9fa99389177
MEGUMMY1 <cys10902@nate.com>
2025-01-13 03:49:02 +0900

    Feat: Header 및 검색창

---
+++ src/components/Layout/Header/Header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./Header.module.scss";
+import IconComponent from "@/components/Asset/Icon";
+import SearchBar from "@/components/SearchBar/SearchBar";
+import Button from "@/components/Button/Button";
+import router from "next/router";
+
+export default function Header() {
+  const handleReload = () => {
+    router.reload();
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.cursor} onClick={handleReload}>
+        <IconComponent name="logo" width={90} height={45} alt="logo" />
+      </div>
+      <SearchBar placeholder="작품 정보를 검색해보세요." />
+      <div className={styles.wrapper}>
+        {/* TODO: 로그인한 유저에게만 보이도록 수정 */}
+        <Button size="s" type="primary">
+          그림 올리기
+        </Button>
+        <div className={styles.cursor}>
+          <IconComponent name="alarm" width={24} height={24} alt="알림" />
+        </div>
+        <div className={styles.cursor}>
+          <IconComponent name="setting" width={24} height={24} alt="설정" />
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./SearchBar.module.scss";
+import { SearchBarProps } from "./SearchBar.types";
+import IconComponent from "../Asset/Icon";
+
+export default function SearchBar({ placeholder }: SearchBarProps) {
+  return (
+    <div className={styles.container}>
+      <input className={styles.input} placeholder={placeholder} />
+      <div className={styles.cursor}>
+        <IconComponent name="magnifier" width={20} height={20} alt="검색" />
+      </div>
+    </div>
+  );
+}
