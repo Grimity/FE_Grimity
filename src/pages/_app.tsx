0bff331aa30c9d6494dd89c6b5e328da8d7a3aa7
MEGUMMY1 <cys10902@nate.com>
2025-01-12 21:11:37 +0900

    Style: global asset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app";
+import "@/styles/globals.scss";
 import "@/styles/reset.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="body">
+      <Component {...pageProps} />
+    </div>
+  );
 }
