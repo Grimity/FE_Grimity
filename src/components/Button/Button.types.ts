8ceb5207fe0ecf3afa1bb092281d8c37b16a9f76
MEGUMMY1 <cys10902@nate.com>
2025-01-15 21:17:06 +0900

    Style: Button 스타일 추가

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,5 +1,10 @@
 import { ReactNode, MouseEventHandler } from "react";
 
+type XSmallButtonProps = {
+  size: "xs";
+  type?: never;
+};
+
 type SmallButtonProps = {
   size: "s";
   type: "primary" | "secondary" | "tertiary";
@@ -14,4 +19,4 @@ export type ButtonProps = {
   children: ReactNode;
   disabled?: boolean;
   onClick?: MouseEventHandler<HTMLButtonElement>;
-} & (SmallButtonProps | LargeButtonProps);
+} & (XSmallButtonProps | SmallButtonProps | LargeButtonProps);
